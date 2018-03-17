cc03af7330aceefcdb1acac33981eb211af8d4af
Gear61 <chessnone@yahoo.com>
2018-03-17 01:17:36 -0700

    Update URL

--- app/src/main/java/com/randomappsinc/simpleloginexample/api/ApiConstants.java
+++ app/src/main/java/com/randomappsinc/simpleloginexample/api/ApiConstants.java
@@ -2,7 +2,7 @@ package com.randomappsinc.simpleloginexample.api;
 
 public class ApiConstants {
 
-    static final String BASE_URL = "https://journeys-rest-api.herokuapp.com";
+    static final String BASE_URL = "https://simple-login-example.herokuapp.com";
 
     public static final int HTTP_STATUS_OK = 200;
 }
